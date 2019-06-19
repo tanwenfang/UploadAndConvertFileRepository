079d6fef2ae13322d100f840f25374c355fcf0ed
tanwenfang <1643327713@qq.com>
2019-06-19 16:25:18 +0800

    first commit

---
+++ UploadAndConvertFile/src/main/java/com/gcb/demo/UploadAndConvertFile/UploadAndConvertFileApplication.java
@@ -0,0 +1,13 @@
+package com.gcb.demo.UploadAndConvertFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadAndConvertFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadAndConvertFileApplication.class, args);
+	}
+
+}
